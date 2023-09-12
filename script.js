cf7e12be73d03d2585ea10798e36ee9dab3ce833
Aaron Kenn Humbert Bondad <aaron.kenn.bondad@gmail.com>
2023-09-12 19:59:22 +0800

    final touches

--- script.js
+++ script.js
@@ -4,36 +4,27 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 const openSavedRecipesBtn = document.getElementById('open-saved-recipes-btn'); // Add this line
 
-// Event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
-// Event listener for the "Open Saved Recipes" button
-openSavedRecipesBtn.addEventListener('click', openSavedRecipesPopup); // Add this line
+// open save recipe
+openSavedRecipesBtn.addEventListener('click', openSavedRecipesPopup);
 
 // splash screen
 window.addEventListener('load', function () {
     // delay 
     setTimeout(function () {
-        // Hide the splash screen
         document.getElementById('splash-screen').style.opacity = '0';
-
-        // Show the app content
         document.getElementById('app-content').style.display = 'block';
-
-        // Optionally, you can add other initialization code for your app here
-
-        // Remove the splash screen from the DOM (optional)
         setTimeout(function () {
             document.getElementById('splash-screen').remove();
-        }, 1000); // Remove after a short delay to allow for the fade-out transition
-    }, 3000); // Simulated 2-second delay (adjust as needed)
+        }, 1000); 
+    }, 3000); 
 });
 
-// Get meal list that matches with the ingredients
 function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
@@ -64,7 +55,7 @@ function getMealList() {
         });
 }
 
-// Get recipe of the meal
+// recipe
 function getMealRecipe(e) {
     e.preventDefault();
     if (e.target.classList.contains('recipe-btn')) {
@@ -74,21 +65,14 @@ function getMealRecipe(e) {
             .then(data => mealRecipeModal(data.meals[0]));
     }
 }
-
-// Event listener for the "Open Saved Recipes" button
 openSavedRecipesBtn.addEventListener('click', openSavedRecipesModal);
-
-// Function to open the popup and display saved recipes
 function openSavedRecipesPopup() {
-    // Retrieve saved recipes from localStorage
     const savedRecipes = JSON.parse(localStorage.getItem('savedRecipes'));
 
     if (savedRecipes && savedRecipes.length > 0) {
         let savedRecipesText = 'Your saved recipes:\n\n';
         savedRecipes.forEach(recipe => {
             savedRecipesText += `Name: ${recipe.name}\nCategory: ${recipe.category}\n`;
-            
-            // Check if the recipe has a video link
             if (recipe.video) {
                 savedRecipesText += `Video Link: ${recipe.video}\n`;
             }
@@ -101,10 +85,7 @@ function openSavedRecipesPopup() {
         alert('You have no saved recipes yet.');
     }
 }
-
-
-
-// Create a modal
+// para sa modal
 function mealRecipeModal(meal) {
     let html = `
         <h2 class="recipe-title">${meal.strMeal}</h2>
@@ -120,8 +101,7 @@ function mealRecipeModal(meal) {
             <a href="${meal.strYoutube}" target="_blank">Video Tutorial Here!</a>
         </div>
     `;
-
-    // Add a Save Recipe button inside the modal
+    // sa loob nung modal
     html += `
         <button class="save-recipe-btn btn" data-id="${meal.idMeal}">
             Save Recipe
@@ -130,14 +110,11 @@ function mealRecipeModal(meal) {
 
     mealDetailsContent.innerHTML = html;
     mealDetailsContent.parentElement.classList.add('showRecipe');
-
-    // Save the recipe to localStorage when the "Save Recipe" button is clicked
+    // local storage
     const saveRecipeBtn = document.querySelector('.save-recipe-btn');
     saveRecipeBtn.addEventListener('click', () => {
-        // Retrieve existing saved recipes from localStorage or initialize an empty array
         const savedRecipes = JSON.parse(localStorage.getItem('savedRecipes')) || [];
 
-        // Add the current recipe to the savedRecipes array
         savedRecipes.push({
             id: meal.idMeal,
             name: meal.strMeal,
@@ -147,7 +124,6 @@ function mealRecipeModal(meal) {
             video: meal.strYoutube,
         });
 
-        // Save the updated savedRecipes array back to localStorage
         localStorage.setItem('savedRecipes', JSON.stringify(savedRecipes));
 
         alert('Recipe saved successfully!');
